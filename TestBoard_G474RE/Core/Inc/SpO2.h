bfeb4e38ec4b2e63e6bc082a9f0771e784ed6764
INVINCIBLE_1NE <laswyun1@kaist.ac.kr>
2025-01-06 21:37:28 +0900

    feat : Example 1까지 구현

--- TestBoard_G474RE/Core/Inc/SpO2.h
+++ TestBoard_G474RE/Core/Inc/SpO2.h
@@ -11,6 +11,7 @@
 #define INC_SPO2_H_
 
 #include "stm32g4xx_hal.h"
+#include "string.h"
 
 // ------------------------------------------- Register Address ------------------------------------------- //
 /* DEVICE ADDRESS */
@@ -111,16 +112,16 @@
 #define MAX30102_MODE_MASK							0xF8U
 #define MAX30102_MODE_REDONLY_HR					0x02U
 #define MAX30102_MODE_REDIRONLY_SPO2				0x03U
-#define MAX30102_MODE_REDIRONLY_MULTILED			0x07U
+#define MAX30102_MODE_MULTILED						0x07U
 
 
 /* PARTICLE SENSING CONFIGURATION */
 #define MAX30102_ADCRANGE_MASK						0x9FU
 
-#define MAX30102_ADCRANGE_11BIT						0x00U			// 7.81,  2048
-#define MAX30102_ADCRANGE_12BIT						0x20U			// 15.63, 4096
-#define MAX30102_ADCRANGE_13BIT						0x40U			// 31.25, 8192
-#define MAX30102_ADCRANGE_14BIT						0x60U			// 62.5,  16384
+#define MAX30102_ADCRANGE_11BIT						0x00U			// 7.81pA per LSB,  2048
+#define MAX30102_ADCRANGE_12BIT						0x20U			// 15.63pA per LSB, 4096
+#define MAX30102_ADCRANGE_13BIT						0x40U			// 31.25pA per LSB, 8192
+#define MAX30102_ADCRANGE_14BIT						0x60U			// 62.5pA per LSB,  16384
 
 #define MAX30102_SAMPLERATE_MASK					0xE3U
 
@@ -156,10 +157,12 @@
 #define MAX_30102_EXPECTEDPARTID					0x15U
 // -------------------------------------------------------------------------------------------------------- //
 
-#define READ_DATA_LENGTH_MAX		4
-#define WRITE_DATA_LENGTH_MAX		1
+#define READ_DATA_LENGTH_MAX		32
+#define WRITE_DATA_LENGTH_MAX		32
 #define I2C_TIMEOUT					1
 #define I2C_MEM_ADD_SIZE			1
+#define I2C_BUFFER_LENGTH 			32
+#define STORAGE_SIZE				4
 
 
 typedef enum _SPO2_State_t {
@@ -167,28 +170,86 @@ typedef enum _SPO2_State_t {
 	SPO2_STATE_ERROR
 } SPO2_State_t;
 
+typedef enum _SPO2_SampleAvg_t {
+	SPO2_SAMPLEAVG_1,
+	SPO2_SAMPLEAVG_2,
+	SPO2_SAMPLEAVG_4,
+	SPO2_SAMPLEAVG_8,
+	SPO2_SAMPLEAVG_16,
+	SPO2_SAMPLEAVG_32
+} SPO2_SampleAvg_t;
 
+typedef enum _SPO2_LEDMODE_t {
+	SPO2_LEDMODE_REDONLY,
+	SPO2_LEDMODE_REDIRONLY,
+	SPO2_LEDMODE_MULTILED
+} SPO2_LEDMode_t;
 
+typedef enum _SPO2_SampleRate_t {
+	SPO2_SAMPLERATE_50,
+	SPO2_SAMPLERATE_100,
+	SPO2_SAMPLERATE_200,
+	SPO2_SAMPLERATE_400,
+	SPO2_SAMPLERATE_800,
+	SPO2_SAMPLERATE_1000,
+	SPO2_SAMPLERATE_1600,
+	SPO2_SAMPLERATE_3200
+} SPO2_SampleRate_t;
 
+typedef enum _SPO2_PulseWidth_t {
+	SPO2_PULSEWIDTH_69,
+	SPO2_PULSEWIDTH_118,
+	SPO2_PULSEWIDTH_215,
+	SPO2_PULSEWIDTH_411
+} SPO2_PulseWidth_t;
+
+typedef enum _SPO2_ADCrange_t {
+	SPO2_ADCRANGE_11bit,
+	SPO2_ADCRANGE_12bit,
+	SPO2_ADCRANGE_13bit,
+	SPO2_ADCRANGE_14bit
+} SPO2_ADCrange_t;
 
 typedef struct _SPO2_Obj_t {
 	I2C_HandleTypeDef* SPO2_i2c;
 	uint8_t devReadAddr;
 	uint8_t devWriteAddr;
-} SPO2_Obj_t;
 
+	uint32_t RED[STORAGE_SIZE];
+	uint32_t IR[STORAGE_SIZE];
+	uint32_t GREEN[STORAGE_SIZE];
+	uint8_t head;
+	uint8_t tail;
+} SPO2_Obj_t;
 
 
 /* Declaration of Functions */
 SPO2_State_t SPO2_Init(SPO2_Obj_t* spo2_Obj, I2C_HandleTypeDef* hi2c);
-uint8_t WriteReg(I2C_HandleTypeDef* hi2c, uint8_t devWriteAddr, uint8_t regAddr);
-uint8_t ReadReg(I2C_HandleTypeDef* hi2c, uint8_t devReadAddr, uint8_t regAddr, uint8_t* readBuff, uint8_t size);
-uint8_t ReadPartID(SPO2_Obj_t* spo2_Obj);
-uint8_t ReadRevisionID(SPO2_Obj_t* spo2_Obj);
-
-
-
-
+void SPO2_SoftReset(SPO2_Obj_t* spo2_Obj);
+void SPO2_SetLEDMode(SPO2_Obj_t* spo2_Obj, uint8_t LEDMode);
+void SPO2_SetADCRange(SPO2_Obj_t* spo2_Obj, uint8_t ADCRange);
+void SPO2_SetSampleRate(SPO2_Obj_t* spo2_Obj, uint8_t sampleRate);
+void SPO2_SetPulseWidth(SPO2_Obj_t* spo2_Obj, uint8_t pulseWidth);
+void SPO2_SetPulseAmpRed(SPO2_Obj_t* spo2_Obj, uint8_t pulseAmp);
+void SPO2_SetPulseAmpIR(SPO2_Obj_t* spo2_Obj, uint8_t pulseAmp);
+void SPO2_SetPulseAmpProximity(SPO2_Obj_t* spo2_Obj, uint8_t pulseAmp);
+void SPO2_EnableSlot(SPO2_Obj_t* spo2_Obj, uint8_t slotNum, uint8_t device);
+void SPO2_SetFIFOAverage(SPO2_Obj_t* spo2_Obj, uint8_t sampleNum);
+void SPO2_ClearFIFO(SPO2_Obj_t* spo2_Obj);
+void SPO2_EnableFIFORollover(SPO2_Obj_t* spo2_Obj);
+void SPO2_DisbleFIFORollover(SPO2_Obj_t* spo2_Obj);
+uint8_t SPO2_GetWritePtr(SPO2_Obj_t* spo2_Obj);
+uint8_t SPO2_GetReadPtr(SPO2_Obj_t* spo2_Obj);
+uint8_t SPO2_ReadPartID(SPO2_Obj_t* spo2_Obj);
+uint8_t SPO2_ReadRevisionID(SPO2_Obj_t* spo2_Obj);
+void SPO2_Setup(SPO2_Obj_t* spo2_Obj, SPO2_SampleAvg_t sampleAvg, SPO2_LEDMode_t LEDMode, SPO2_ADCrange_t ADCrange, SPO2_SampleRate_t sampleRate, SPO2_PulseWidth_t pulseWidth, uint8_t powerLevel);
+uint32_t SPO2_GetRED(SPO2_Obj_t* spo2_Obj);
+uint32_t SPO2_GetIR(SPO2_Obj_t* spo2_Obj);
+uint16_t SPO2_Check(SPO2_Obj_t* spo2_Obj);
+uint8_t SPO2_SafeCheck(SPO2_Obj_t* spo2_Obj, uint32_t timeOut);
+void SPO2_BitMask(SPO2_Obj_t* spo2_Obj, uint8_t regAddr, uint8_t mask, uint8_t setBit);
+uint8_t SPO2_ReadReg(I2C_HandleTypeDef* hi2c, uint8_t devReadAddr, uint8_t regAddr, uint8_t* readBuff, uint8_t size);
+uint8_t SPO2_WriteReg(I2C_HandleTypeDef* hi2c, uint8_t devWriteAddr, uint8_t regAddr, uint8_t* writeBuff, uint8_t size);
 
 
 
--- TestBoard_G474RE/Core/Src/SpO2.c
+++ TestBoard_G474RE/Core/Src/SpO2.c
@@ -10,47 +10,526 @@
 /* I2C Received Data */
 static uint8_t I2CRxData[READ_DATA_LENGTH_MAX] = {0};
 static uint8_t I2CTxData[WRITE_DATA_LENGTH_MAX] = {0};
+float sysMHz = 170;		// Change this value according to MCU
+uint8_t activeLEDNum = 0;
 
 
+/* Initialize the sensor */
 SPO2_State_t SPO2_Init(SPO2_Obj_t* spo2_Obj, I2C_HandleTypeDef* hi2c)
 {
 	spo2_Obj->SPO2_i2c = hi2c;
 	spo2_Obj->devReadAddr = MAX30102_I2C_READ_ADDR;
 	spo2_Obj->devWriteAddr = MAX30102_I2C_WRITE_ADDR;
 
-	if (ReadPartID(spo2_Obj) != MAX_30102_EXPECTEDPARTID) {
+	if (SPO2_ReadPartID(spo2_Obj) != MAX_30102_EXPECTEDPARTID) {
 		return SPO2_STATE_ERROR;
 	}
 
-	ReadRevisionID(spo2_Obj);
+	SPO2_ReadRevisionID(spo2_Obj);
 
 	return SPO2_STATE_OK;
 }
 
 
-SPO2_State_t SPO2_Setup(SPO2_Obj_t* spo2_Obj, )
+/* Reset the sensor */
+void SPO2_SoftReset(SPO2_Obj_t* spo2_Obj)
 {
+	SPO2_BitMask(spo2_Obj, MAX30102_MODE_CONFIG, MAX30102_MODE_RESET_MASK, MAX30102_MODE_RESET_ENABLE);
 
-}
+	float usStart = (float)DWT->CYCCNT / sysMHz;
 
+	while ( (float)DWT->CYCCNT/sysMHz - usStart < 100000) {
+		uint8_t state = 0;
+		uint8_t rxData = 0;
+		state = SPO2_ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devReadAddr, MAX30102_MODE_CONFIG, I2CRxData, 1);
 
-void bitMask(SPO2_Obj_t* spo2_Obj, uint8_t regAddr, uint8_t mask, uint8_t thing)
-{
-	uint8_t originalData = 0;
-
-	if (ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devReadAddr, regAddr, I2CRxData, 1) == 0){
-		originalData = I2CRxData[0];
-
-		originalData = originalData & mask;
-
-
-		I2CTxData = originalData | thing
-		WriteReg(spo2_Obj->SPO2_i2c, spo2_Obj->devWriteAddr, regAddr, I2CTxData, 1)
+		if (state == 0) {
+			rxData = I2CRxData[0];
+			if ((rxData & MAX30102_MODE_RESET_ENABLE) == 0) {
+				break;
+				HAL_Delay(1);
+			}
+		}
 	}
 }
 
 
-uint8_t WriteReg(I2C_HandleTypeDef* hi2c, uint8_t devWriteAddr, uint8_t regAddr, uint8_t* writeBuff, uint8_t size)
+/* Set the LED mode */
+/* [NOTE] RED only, RED + IR, Multi-LED */
+void SPO2_SetLEDMode(SPO2_Obj_t* spo2_Obj, uint8_t LEDMode)
+{
+	SPO2_BitMask(spo2_Obj, MAX30102_MODE_CONFIG, MAX30102_MODE_MASK, LEDMode);
+}
+
+
+/* Set the ADC range */
+/* [NOTE] ADC Range : 2048(11-bit), 4096(12-bit), 8192(13-bit), 16384(14-bit) */
+void SPO2_SetADCRange(SPO2_Obj_t* spo2_Obj, uint8_t ADCRange)
+{
+	SPO2_BitMask(spo2_Obj, MAX30102_SPO2_CONFIG, MAX30102_ADCRANGE_MASK, ADCRange);
+}
+
+
+/* Set the sample rate */
+/* [NOTE] Sample Rate : 50, 100, 200, 400, 800, 1000, 1600, 3200 */
+void SPO2_SetSampleRate(SPO2_Obj_t* spo2_Obj, uint8_t sampleRate)
+{
+	SPO2_BitMask(spo2_Obj, MAX30102_SPO2_CONFIG, MAX30102_SAMPLERATE_MASK, sampleRate);
+}
+
+
+/* Set the pulse width */
+/* [NOTE] Pulse Width : 69, 118, 215, 411 */
+void SPO2_SetPulseWidth(SPO2_Obj_t* spo2_Obj, uint8_t pulseWidth)
+{
+	SPO2_BitMask(spo2_Obj, MAX30102_SPO2_CONFIG, MAX30102_PULSEWIDTH_MASK, pulseWidth);
+}
+
+
+/* Set the amplitude of RED pulse */
+/* [NOTE] Amplitude values: 0x00 = 0mA, 0x7F = 25.4mA, 0xFF = 50mA (typical) */
+void SPO2_SetPulseAmpRed(SPO2_Obj_t* spo2_Obj, uint8_t pulseAmp)
+{
+	I2CTxData[0] = pulseAmp;
+	SPO2_WriteReg(spo2_Obj->SPO2_i2c, spo2_Obj->devWriteAddr, MAX30102_LED1_PULSEAMP, I2CTxData, 1);
+}
+
+
+/* Set the amplitude of IR pulse */
+void SPO2_SetPulseAmpIR(SPO2_Obj_t* spo2_Obj, uint8_t pulseAmp)
+{
+	I2CTxData[0] = pulseAmp;
+	SPO2_WriteReg(spo2_Obj->SPO2_i2c, spo2_Obj->devWriteAddr, MAX30102_LED2_PULSEAMP, I2CTxData, 1);
+}
+
+
+/* Set the amplitude of proximity pulse */
+void SPO2_SetPulseAmpProximity(SPO2_Obj_t* spo2_Obj, uint8_t pulseAmp)
+{
+	I2CTxData[0] = pulseAmp;
+	SPO2_WriteReg(spo2_Obj->SPO2_i2c, spo2_Obj->devWriteAddr, MAX30102_PROXIMITY_PULSEAMP, I2CTxData, 1);
+}
+
+
+/* Assign the slots for each device(RED, IR, GREEN) */
+void SPO2_EnableSlot(SPO2_Obj_t* spo2_Obj, uint8_t slotNum, uint8_t device)
+{
+	switch (slotNum) {
+		case (1):
+			SPO2_BitMask(spo2_Obj, MAX30102_MULTILED_CONTROL_1, MAX30102_SLOT1_MASK, device);
+			break;
+		case (2):
+			SPO2_BitMask(spo2_Obj, MAX30102_MULTILED_CONTROL_1, MAX30102_SLOT2_MASK, device<<4);
+			break;
+		case (3):
+			SPO2_BitMask(spo2_Obj, MAX30102_MULTILED_CONTROL_2, MAX30102_SLOT3_MASK, device);
+			break;
+		case (4):
+			SPO2_BitMask(spo2_Obj, MAX30102_MULTILED_CONTROL_2, MAX30102_SLOT4_MASK, device<<4);
+			break;
+		default:
+			/* If the code goes into here, process has an error */
+			break;
+	}
+}
+
+
+/* Set sample average */
+void SPO2_SetFIFOAverage(SPO2_Obj_t* spo2_Obj, uint8_t sampleNum)
+{
+	SPO2_BitMask(spo2_Obj, MAX30102_FIFO_CONFIG, MAX30102_FIFO_SAMPLEAVG_MASK, sampleNum);
+}
+
+
+/* Clearing FIFO after "finishing SETUP" before "starting to read" */
+void SPO2_ClearFIFO(SPO2_Obj_t* spo2_Obj)
+{
+	I2CTxData[0] = 0b00000000;
+	SPO2_WriteReg(spo2_Obj->SPO2_i2c, spo2_Obj->devWriteAddr, MAX30102_FIFO_WRITE_POINTER, I2CTxData, 1);
+	SPO2_WriteReg(spo2_Obj->SPO2_i2c, spo2_Obj->devWriteAddr, MAX30102_OVERFLOW_COUNTER, I2CTxData, 1);
+	SPO2_WriteReg(spo2_Obj->SPO2_i2c, spo2_Obj->devWriteAddr, MAX30102_FIFO_READ_POINTER, I2CTxData, 1);
+}
+
+
+/* Enable Roll-over if FIFO overflows */
+void SPO2_EnableFIFORollover(SPO2_Obj_t* spo2_Obj)
+{
+	SPO2_BitMask(spo2_Obj, MAX30102_FIFO_CONFIG, MAX30102_FIFO_ROLLOVER_MASK, MAX30102_FIFO_ROLLOVER_ENABLE);
+}
+
+
+/* Disable Roll-over */
+void SPO2_DisbleFIFORollover(SPO2_Obj_t* spo2_Obj)
+{
+	SPO2_BitMask(spo2_Obj, MAX30102_FIFO_CONFIG, MAX30102_FIFO_ROLLOVER_MASK, MAX30102_FIFO_ROLLOVER_DISABLE);
+}
+
+
+/* Get Write Pointer */
+uint8_t SPO2_GetWritePtr(SPO2_Obj_t* spo2_Obj)
+{
+	uint8_t state = 0;
+	uint8_t writePtr = 0;
+
+	state = SPO2_ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devReadAddr, MAX30102_FIFO_WRITE_POINTER, I2CRxData, 1);
+
+	if (state == 0) {
+		writePtr = I2CRxData[0];
+		return writePtr;
+	}
+
+	/* Needs error handler */
+	return writePtr;
+}
+
+
+/* Get Read Pointer */
+uint8_t SPO2_GetReadPtr(SPO2_Obj_t* spo2_Obj)
+{
+	uint8_t state = 0;
+	uint8_t readPtr = 0;
+
+	state = SPO2_ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devReadAddr, MAX30102_FIFO_READ_POINTER, I2CRxData, 1);
+
+	if (state == 0) {
+		readPtr = I2CRxData[0];
+		return readPtr;
+	}
+
+	/* Needs error handler */
+	return readPtr;
+}
+
+
+/* Get PART ID */
+uint8_t SPO2_ReadPartID(SPO2_Obj_t* spo2_Obj)
+{
+	uint8_t state = 0;
+	uint8_t partID = 0;
+
+	state = SPO2_ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devReadAddr, MAX30102_PART_ID, I2CRxData, 1);
+
+	if (state == 0){
+		partID = I2CRxData[0];
+	}
+
+	return partID;
+}
+
+
+/* Get REVISION ID */
+uint8_t SPO2_ReadRevisionID(SPO2_Obj_t* spo2_Obj)
+{
+	uint8_t state = 0;
+	uint8_t revisionID = 0;
+
+	state = SPO2_ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devReadAddr, MAX30102_REVISION_ID, I2CRxData, 1);
+
+	if (state == 0){
+		revisionID = I2CRxData[0];
+	}
+
+	return revisionID;
+}
+
+
+/* Setup the sensor before the starting of reading */
+void SPO2_Setup(SPO2_Obj_t* spo2_Obj, SPO2_SampleAvg_t sampleAvg, SPO2_LEDMode_t LEDMode, SPO2_ADCrange_t ADCrange, SPO2_SampleRate_t sampleRate, SPO2_PulseWidth_t pulseWidth, uint8_t powerLevel)
+{
+	SPO2_SoftReset(spo2_Obj);
+
+	/* ------------------------------ FIFO Configuration ------------------------------ */
+	if (sampleAvg == SPO2_SAMPLEAVG_1) {
+		SPO2_SetFIFOAverage(spo2_Obj, MAX30102_FIFO_SAMPLEAVG_1);
+	}
+	else if (sampleAvg == SPO2_SAMPLEAVG_2) {
+		SPO2_SetFIFOAverage(spo2_Obj, MAX30102_FIFO_SAMPLEAVG_2);
+	}
+	else if (sampleAvg == SPO2_SAMPLEAVG_4) {
+		SPO2_SetFIFOAverage(spo2_Obj, MAX30102_FIFO_SAMPLEAVG_4);
+	}
+	else if (sampleAvg == SPO2_SAMPLEAVG_8) {
+		SPO2_SetFIFOAverage(spo2_Obj, MAX30102_FIFO_SAMPLEAVG_8);
+	}
+	else if (sampleAvg == SPO2_SAMPLEAVG_16) {
+		SPO2_SetFIFOAverage(spo2_Obj, MAX30102_FIFO_SAMPLEAVG_16);
+	}
+	else if (sampleAvg == SPO2_SAMPLEAVG_32) {
+		SPO2_SetFIFOAverage(spo2_Obj, MAX30102_FIFO_SAMPLEAVG_32);
+	}
+	else {
+		SPO2_SetFIFOAverage(spo2_Obj, MAX30102_FIFO_SAMPLEAVG_4);
+	}
+	/* -------------------------------------------------------------------------------- */
+
+
+	/* ---------------------------- Enable FIFO Roll over ----------------------------- */
+	SPO2_EnableFIFORollover(spo2_Obj);
+	/* -------------------------------------------------------------------------------- */
+
+
+	/* ------------------------------ Mode Configuration ------------------------------ */
+	if (LEDMode == SPO2_LEDMODE_MULTILED) {
+		activeLEDNum = 3;
+		SPO2_SetLEDMode(spo2_Obj, MAX30102_MODE_MULTILED);
+	}
+	else if (LEDMode == SPO2_LEDMODE_REDIRONLY) {
+		activeLEDNum = 2;
+		SPO2_SetLEDMode(spo2_Obj, MAX30102_MODE_REDIRONLY_SPO2);
+	}
+	else if (LEDMode == SPO2_LEDMODE_REDONLY) {
+		activeLEDNum = 1;
+		SPO2_SetLEDMode(spo2_Obj, MAX30102_MODE_REDONLY_HR);
+	}
+	else {
+		activeLEDNum = 1;
+		SPO2_SetLEDMode(spo2_Obj, MAX30102_MODE_REDONLY_HR);
+	}
+	/* -------------------------------------------------------------------------------- */
+
+
+	/* ------------------------ Particle Sensing Configuration ------------------------ */
+	// ADC Range //
+	if (ADCrange == SPO2_ADCRANGE_11bit) {
+		SPO2_SetADCRange(spo2_Obj, MAX30102_ADCRANGE_11BIT);
+	}
+	else if (ADCrange == SPO2_ADCRANGE_12bit) {
+		SPO2_SetADCRange(spo2_Obj, MAX30102_ADCRANGE_12BIT);
+	}
+	else if (ADCrange == SPO2_ADCRANGE_13bit) {
+		SPO2_SetADCRange(spo2_Obj, MAX30102_ADCRANGE_13BIT);
+	}
+	else if (ADCrange == SPO2_ADCRANGE_14bit) {
+		SPO2_SetADCRange(spo2_Obj, MAX30102_ADCRANGE_14BIT);
+	}
+	else {
+		SPO2_SetADCRange(spo2_Obj, MAX30102_ADCRANGE_11BIT);
+	}
+
+
+	// Sample Rate //
+	if (sampleRate == SPO2_SAMPLERATE_50) {
+		SPO2_SetSampleRate(spo2_Obj, MAX30102_SAMPLERATE_50);
+	}
+	else if (sampleRate == SPO2_SAMPLERATE_100) {
+		SPO2_SetSampleRate(spo2_Obj, MAX30102_SAMPLERATE_100);
+	}
+	else if (sampleRate == SPO2_SAMPLERATE_200) {
+		SPO2_SetSampleRate(spo2_Obj, MAX30102_SAMPLERATE_200);
+	}
+	else if (sampleRate == SPO2_SAMPLERATE_400) {
+		SPO2_SetSampleRate(spo2_Obj, MAX30102_SAMPLERATE_400);
+	}
+	else if (sampleRate == SPO2_SAMPLERATE_800) {
+		SPO2_SetSampleRate(spo2_Obj, MAX30102_SAMPLERATE_800);
+	}
+	else if (sampleRate == SPO2_SAMPLERATE_1000) {
+		SPO2_SetSampleRate(spo2_Obj, MAX30102_SAMPLERATE_1000);
+	}
+	else if (sampleRate == SPO2_SAMPLERATE_1600) {
+		SPO2_SetSampleRate(spo2_Obj, MAX30102_SAMPLERATE_1600);
+	}
+	else if (sampleRate == SPO2_SAMPLERATE_3200) {
+		SPO2_SetSampleRate(spo2_Obj, MAX30102_SAMPLERATE_3200);
+	}
+	else {
+		SPO2_SetSampleRate(spo2_Obj, MAX30102_SAMPLERATE_50);
+	}
+
+
+	// Pulse Width //
+	  /* The longer the pulse width the longer range of detection you'll have
+	     At 69us and 0.4mA it's about 2 inches
+	     At 411us and 0.4mA it's about 6 inches */
+	if (pulseWidth == SPO2_PULSEWIDTH_69) {
+		SPO2_SetPulseWidth(spo2_Obj, MAX30102_PULSEWIDTH_69);
+	}
+	else if (pulseWidth == SPO2_PULSEWIDTH_118) {
+		SPO2_SetPulseWidth(spo2_Obj, MAX30102_PULSEWIDTH_118);
+	}
+	else if (pulseWidth == SPO2_PULSEWIDTH_215) {
+		SPO2_SetPulseWidth(spo2_Obj, MAX30102_PULSEWIDTH_215);
+	}
+	else if (pulseWidth == SPO2_PULSEWIDTH_411) {
+		SPO2_SetPulseWidth(spo2_Obj, MAX30102_PULSEWIDTH_411);
+	}
+	else {
+		SPO2_SetPulseWidth(spo2_Obj, MAX30102_PULSEWIDTH_69);
+	}
+	/* -------------------------------------------------------------------------------- */
+
+
+	/* ----------------------- LED Pulse Amplitude Configuration ---------------------- */
+		/* Default is 0x1F which gets us 6.4mA
+		   powerLevel = 0x02, 0.4mA - Presence detection of ~4 inch
+	 	   powerLevel = 0x1F, 6.4mA - Presence detection of ~8 inch
+		   powerLevel = 0x7F, 25.4mA - Presence detection of ~8 inch
+		   powerLevel = 0xFF, 50.0mA - Presence detection of ~12 inch */
+	SPO2_SetPulseAmpRed(spo2_Obj, powerLevel);
+	SPO2_SetPulseAmpIR(spo2_Obj, powerLevel);
+	SPO2_SetPulseAmpProximity(spo2_Obj, powerLevel);
+	/* -------------------------------------------------------------------------------- */
+
+
+	/* ---------- Multi-LED Mode Configuration (Enable the reading of 2 LEDs) --------- */
+	SPO2_EnableSlot(spo2_Obj, 1, MAX30102_SLOT_RED_LED);
+	if (LEDMode >= 1) {
+		SPO2_EnableSlot(spo2_Obj, 2, MAX30102_SLOT_IR_LED);
+	}
+	/* -------------------------------------------------------------------------------- */
+
+
+	/* -------------------------------- Reset the FIFO -------------------------------- */
+	SPO2_ClearFIFO(spo2_Obj);
+	/* -------------------------------------------------------------------------------- */
+}
+
+
+/* Take the recent RED value */
+uint32_t SPO2_GetRED(SPO2_Obj_t* spo2_Obj)
+{
+	// Check the sensor for new data for 250,000usec(=250ms)
+	if(SPO2_SafeCheck(spo2_Obj, 250000))
+		return (spo2_Obj->RED[spo2_Obj->head]);
+	else
+		return(0); // Sensor failed to find new data
+}
+
+
+/* Take the recent IR value */
+uint32_t SPO2_GetIR(SPO2_Obj_t* spo2_Obj)
+{
+	//Check the sensor for new data for 250,000usec(=250ms)
+	if(SPO2_SafeCheck(spo2_Obj, 250000))
+		return (spo2_Obj->IR[spo2_Obj->head]);
+	else
+		return(0); // Sensor failed to find new data
+}
+
+
+/* Polling the new sensor data */
+uint16_t SPO2_Check(SPO2_Obj_t* spo2_Obj)
+{
+	uint8_t readPtr = SPO2_GetReadPtr(spo2_Obj);
+	uint8_t writePtr = SPO2_GetWritePtr(spo2_Obj);
+
+	int sampleNum = 0;
+
+	if (readPtr != writePtr) {
+		sampleNum = writePtr - readPtr;
+		if (sampleNum < 0) {
+			sampleNum += 32;
+		}
+
+		int bytesLeftToRead = sampleNum * activeLEDNum * 3;
+
+		while (bytesLeftToRead > 0) {
+			int toGet = bytesLeftToRead;
+
+			if (toGet > I2C_BUFFER_LENGTH) {
+				toGet = I2C_BUFFER_LENGTH - (I2C_BUFFER_LENGTH % (activeLEDNum * 3));
+			}
+
+			bytesLeftToRead -= toGet;
+			SPO2_ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devWriteAddr, MAX30102_FIFO_DATA_REGISTER, I2CRxData, toGet);
+
+			while (toGet > 0) {
+				static uint8_t cursor = 0;
+
+				spo2_Obj->head++;
+				spo2_Obj->head %= STORAGE_SIZE;
+
+				uint8_t temp[4];
+				uint32_t tempLong;
+
+				// Burst Read 3 bytes for RED //
+				temp[3] = 0;
+				temp[2] = I2CRxData[cursor];
+				temp[1] = I2CRxData[cursor+1];
+				temp[0] = I2CRxData[cursor+1];
+
+				memcpy(&tempLong, temp, sizeof(tempLong));
+				tempLong &= 0x3FFFF;	// use only 18-bit
+
+				spo2_Obj->RED[spo2_Obj->head] = tempLong;
+
+				if (activeLEDNum > 1) {
+					// Burst Read 3 bytes for IR //
+					temp[3] = 0;
+					temp[2] = I2CRxData[cursor+1];
+					temp[1] = I2CRxData[cursor+1];
+					temp[0] = I2CRxData[cursor+1];
+
+					memcpy(&tempLong, temp, sizeof(tempLong));
+					tempLong &= 0x3FFFF;	// use only 18-bit
+					spo2_Obj->IR[spo2_Obj->head] = tempLong;
+				}
+
+				if (activeLEDNum > 2) {
+					// Burst Read 3 bytes for GREEN //
+					temp[3] = 0;
+					temp[2] = I2CRxData[cursor+1];
+					temp[1] = I2CRxData[cursor+1];
+					temp[0] = I2CRxData[cursor+1];
+
+					memcpy(&tempLong, temp, sizeof(tempLong));
+					tempLong &= 0x3FFFF;	// use only 18-bit
+					spo2_Obj->GREEN[spo2_Obj->head] = tempLong;
+				}
+
+				toGet -= activeLEDNum * 3;
+			}
+		}
+	}
+
+	return sampleNum;		// Return total number of new samples
+}
+
+
+/* Check the new data */
+uint8_t SPO2_SafeCheck(SPO2_Obj_t* spo2_Obj, uint32_t timeOut)
+{
+	float usStart = (float)DWT->CYCCNT / sysMHz;
+	while (1) {
+		if ((float)DWT->CYCCNT/sysMHz - usStart > timeOut) {
+			return 0;
+		}
+		if (SPO2_Check(spo2_Obj) > 0)
+			return 1;
+
+		HAL_Delay(1);
+	}
+}
+
+
+/* Masking & Setting the value of certain register */
+void SPO2_BitMask(SPO2_Obj_t* spo2_Obj, uint8_t regAddr, uint8_t mask, uint8_t setBit)
+{
+	uint8_t originalData = 0;
+
+	if (SPO2_ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devReadAddr, regAddr, I2CRxData, 1) == 0){
+		originalData = I2CRxData[0];
+
+		originalData = originalData & mask;		// Makes certain portion of register value zero.
+
+		I2CTxData[0] = originalData | setBit;
+		SPO2_WriteReg(spo2_Obj->SPO2_i2c, spo2_Obj->devWriteAddr, regAddr, I2CTxData, 1);
+	}
+}
+
+
+/* I2C READ function */
+uint8_t SPO2_ReadReg(I2C_HandleTypeDef* hi2c, uint8_t devReadAddr, uint8_t regAddr, uint8_t* readBuff, uint8_t size)
+{
+	uint8_t state = 0;
+
+	state = HAL_I2C_Mem_Read(hi2c, devReadAddr, regAddr, I2C_MEM_ADD_SIZE, readBuff, size, I2C_TIMEOUT);
+
+	return state;
+}
+
+/* I2C WRITE function */
+uint8_t SPO2_WriteReg(I2C_HandleTypeDef* hi2c, uint8_t devWriteAddr, uint8_t regAddr, uint8_t* writeBuff, uint8_t size)
 {
 	uint8_t state = 0;
 
@@ -60,42 +539,6 @@ uint8_t WriteReg(I2C_HandleTypeDef* hi2c, uint8_t devWriteAddr, uint8_t regAddr,
 }
 
 
-uint8_t ReadReg(I2C_HandleTypeDef* hi2c, uint8_t devReadAddr, uint8_t regAddr, uint8_t* readBuff, uint8_t size)
-{
-	uint8_t state = 0;
-
-	state = HAL_I2C_Mem_Read(hi2c, devReadAddr, regAddr, I2C_MEM_ADD_SIZE, readBuff, size, I2C_TIMEOUT);
-
-	return state;
-}
-
-uint8_t ReadPartID(SPO2_Obj_t* spo2_Obj)
-{
-	uint8_t state = 0;
-	uint8_t partID = 0;
-
-	state = ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devReadAddr, MAX30102_PART_ID, I2CRxData, 1);
-
-	if (state == 0){
-		partID = I2CRxData[0];
-	}
-
-	return partID;
-}
-
-uint8_t ReadRevisionID(SPO2_Obj_t* spo2_Obj)
-{
-	uint8_t state = 0;
-	uint8_t revisionID = 0;
-
-	state = ReadReg(spo2_Obj->SPO2_i2c, spo2_Obj->devReadAddr, MAX30102_REVISION_ID, I2CRxData, 1);
-
-	if (state == 0){
-		revisionID = I2CRxData[0];
-	}
-
-	return revisionID;
-}
 
 
 
